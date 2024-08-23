bd55940c0c606c0c06a27d8673c1a9738d9f3055
Conor Venus <conorvenus@gmail.com>
2024-08-23 08:41:01 +0100

    Initial commit

---
+++ upload-service/main.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+from git import Repo
+import threading
+import uuid
+import os
+import boto3
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import redis
+
+app = Flask(__name__)
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app
+)
+
+session = boto3.Session()
+
+s3 = session.client(
+    service_name='s3',
+    endpoint_url='http://127.0.0.1:9000',
+    aws_access_key_id=os.environ['S3_ACCESS_KEY'],
+    aws_secret_access_key=os.environ['S3_SECRET_KEY'],
+    aws_session_token=None,
+    config=boto3.session.Config(signature_version='s3v4'),
+    verify=False
+)
+
+statuses = {}
+
+redis = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+def get_files(dir):
+    files = []
+    for root, _, filenames in os.walk(dir):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
+
+def clone_repo(repo_url, id):
+    try:
+        Repo.clone_from(repo_url, f"repos/{id}")
+        for file in get_files(f"repos/{id}"):
+            if ".git" in file:
+                continue
+            file = file.replace("\\", "/")
+            s3.upload_file(file, "testbucket", file)
+            print(f"Uploaded {file}")
+            os.remove(file)
+        statuses[id] = "completed"
+        redis.lpush("completed_uploads", id)
+    except Exception as e:
+        statuses[id] = "failed"
+
+def get_uuid():
+    return str(uuid.uuid4())
+
+@app.route("/deploy", methods=['GET'])
+@limiter.limit("1 per 10 seconds")
+def deploy():
+    repo_url = request.args.get('repoUrl')
+    if not repo_url:
+        return jsonify({"error": "No repoUrl provided"}), 400
+    id = get_uuid()
+    statuses[id] = "pending"
+    threading.Thread(target=clone_repo, args=(repo_url, id)).start()
+    return jsonify({"id": id}), 200
+
+@app.route("/status", methods=['GET'])
+def status():
+    id = request.args.get('id')
+    if not id:
+        return jsonify({"error": "No id provided"}), 400
+    if id in statuses:
+        return jsonify({"status": statuses[id]}), 200
+    return jsonify({"error": "No deployment found with the provided id"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
