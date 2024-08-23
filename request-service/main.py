8bcc3e15dd697b18739d85e9ee94a245c08f244c
Conor Venus <conorvenus@gmail.com>
2024-08-23 16:02:25 +0100

    Changed endpoint url to use .env

--- request-service/main.py
+++ request-service/main.py
@@ -12,7 +12,7 @@ session = boto3.Session()
 
 s3 = session.client(
     service_name='s3',
-    endpoint_url='http://127.0.0.1:9000',
+    endpoint_url=os.environ['S3_URL'],
     aws_access_key_id=os.environ['S3_ACCESS_KEY'],
     aws_secret_access_key=os.environ['S3_SECRET_KEY'],
     aws_session_token=None,
